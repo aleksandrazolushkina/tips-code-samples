21d348b71451f6f231bf57b580ea85c9caf7b6e2
Mikhail Kuznetsov <mikhail.kuznetsov@jetbrains.com>
2021-07-23 16:50:13 +0300

    reordering

--- src/com/jetbrains/testing/SomeMethods.java
+++ src/com/jetbrains/testing/SomeMethods.java
@@ -1,17 +1,16 @@
 package com.jetbrains.testing;
-
 import com.jetbrains.service.Person;
 
 public class SomeMethods {
+
+    public void someMethod(){
+        String text = "Some text";
+        System.out.println(text);
+    }
 
     public void usingPerson(){
         Person person = new Person("John", "Doe");
         System.out.println(person.getGreeting());
         System.out.println("My name is " + person.name + " " + person.surname);
-    }
-
-    public void someMethod(){
-        String text = "Some text";
-        System.out.println(text);
     }
 }
