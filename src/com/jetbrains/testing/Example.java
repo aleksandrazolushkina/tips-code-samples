2851489f30fad8dcac0c9ab5a9cb5a6fffe2dd9f
Mikhail Kuznetsov <mikhail.kuznetsov@jetbrains.com>
2021-08-17 15:50:04 +0300

    new test method

---
+++ src/com/jetbrains/testing/Example.java
@@ -0,0 +1,11 @@
+package com.jetbrains.testing;
+
+import com.jetbrains.inheritance.Beaver;
+
+public class Example {
+
+    public void createAnimals(){
+        Beaver beaver = new Beaver();
+        Cat cat = new Cat();
+    }
+}
