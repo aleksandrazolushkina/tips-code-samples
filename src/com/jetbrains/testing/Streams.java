ab43ace3433332e0470d624a791ae342b0f5005d
Mikhail Kuznetsov <mikhail.kuznetsov@jetbrains.com>
2021-08-17 13:40:09 +0300

    stream completion method update

--- src/com/jetbrains/testing/Streams.java
+++ src/com/jetbrains/testing/Streams.java
@@ -1,10 +1,12 @@
 package com.jetbrains.testing;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class Streams {
 
-    public void completionSample(List<String> someList){
+    public void completionSample() {
+        List<String> someList = Arrays.asList("one", "two", "three");
         someList
     }
 }
