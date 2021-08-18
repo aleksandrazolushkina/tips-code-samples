6e19dd72f4408b821ba697f86d058dae9a3827fa
Mikhail Kuznetsov <mikhail.kuznetsov@jetbrains.com>
2021-08-18 18:46:21 +0300

    new class for completions

---
+++ src/com/jetbrains/testing/CompletionExamples.java
@@ -0,0 +1,11 @@
+package com.jetbrains.testing;
+
+import java.io.*;
+
+public class CompletionExamples {
+
+    public void smartType() {
+        BufferedReader br;
+        br = new BufferedReader();
+    }
+}
