89050ad14627be8cde80b28b5724fc95cafc743e
Mikhail Kuznetsov <mikhail.kuznetsov@jetbrains.com>
2021-10-21 15:16:07 +0300

    class with problems

---
+++ src/com/jetbrains/testing/SomeClass.java
@@ -0,0 +1,14 @@
+package com.jetbrains.testing;
+
+public class SomeClass {
+
+    int someIntValue = "string";
+
+    private boolean foo(){
+        return true;
+    }
+
+    private void fooUsing(){
+        foo();
+    }
+}
